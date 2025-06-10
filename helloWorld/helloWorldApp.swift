5c3c823bbc8fc79454b93a2a802ce998327a22aa
Tarush Sinha <tarushsinha@mac.lan>
2025-06-10 14:58:24 -0700

    first commit of CalorieBro

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Tarush Sinha on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ helloWorldUITests/helloWorldUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  helloWorldUITestsLaunchTests.swift
+//  helloWorldUITests
+//
+//  Created by Tarush Sinha on 6/10/25.
+//
+
+import XCTest
+
+final class helloWorldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
